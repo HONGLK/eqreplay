1f621b33e9389c4007fc475a23814eb3575189de
Lambert Chen <lambertchen@dps.com.tw>
2021-04-22 18:29:36 +0800

    add sites and s wave.

--- load_data.py
+++ load_data.py
@@ -1,6 +1,7 @@
 import json
-def load_data():
-    with open("testdata.json", "r", encoding='utf-8') as d:
+import pandas as pd
+def load_data(filename):
+    with open(filename, "r", encoding='utf-8') as d:
         data = json.loads(d.read())
         #for i in data:
             #data_structure = []
@@ -11,3 +12,19 @@ def load_data():
             #    Intensity = j["Intensity"]
             #    PGA = j["PGA"]
     return data
+ 
+def load_site(filename):
+    with open(filename, "r", encoding="utf-8") as d:
+        data = json.loads(d.read())
+    site_data = pd.DataFrame(columns=["Site_Name", "Site_ID", "Site_Lat", "Site_Lon", "Site_Level", "PGAx", "Datetime"])
+
+    for i in data:
+        Date = i["Date"]
+        Time = i["Time"]
+        for site in i["Sites"]:
+            li = list(site.values())
+            li.append(str(Date+" "+Time))
+            df = pd.Series((li), index=site_data.columns)
+            site_data = site_data.append(df, ignore_index=True)
+
+    return site_data
