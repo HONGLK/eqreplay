9a7c0b8661e5c09a62af6425d0a9072a299bbb34
HONGLK <andy851220@gmail.com>
2022-08-12 13:44:19 +0800

    fixing structure.

---
+++ Data/Test.py
@@ -0,0 +1,8 @@
+import json
+from datetime import datetime
+import sys
+sys.path.insert(0, '..\\Source\\')
+from EQ_Animation_functionalized import Animation
+
+a = Animation('2022_06_28')
+a.draw()
